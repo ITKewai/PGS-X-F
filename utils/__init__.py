8f152ba4a9ce9dc42956324baf3653e08df232fd
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2025-10-10 09:52:36 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,4 +5,6 @@
 # TODO: verifiche (verificare che per i reset pressostati si scende solo con il joystick, se ci sono digitali nel fb_err)
 # TODO: veridicare che la variabile di sistema sia su "SI" il booleano in caso di assi se no non VA (L LL L0 EEC)
 # TODO: caricare tutte le informazioni asse per asse
+# TODO: controllo per vedere se un uscita DO o AO è usata su piu assi
+# TODO: controllo se c è un SYSTEM di usasse usato ma c'è il FLAG su NO
 """

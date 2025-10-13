3f6727c166e1ea7e0e13fc2d6643fac825e26a16
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2025-10-13 17:55:45 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -10,4 +10,12 @@
 # TODO: controllo safety interlock sgancio chiusura, non deve esserci Droll L (o la system)
 # TODO: ricarca Freee daindici sbagliati
 @ TODO:  ricerca che negli assi non rimangono indici in opt param
+# TODO: controllo che la quota H sia maggiore di HH nel pinzaggio se no non apri sgancio in automatico
+@# TODO: controllare che sui rulli laterali MAN SP UP sia disattivo
+TODO: controllo che tutti i system usati degli assi abbiamo "YES"al flag se no non vanno
+TODO: notificare se le termiche sono state inserite nei motori (no resistanza(
+TODO: nella ricerca dei DI ci sono anche RECICLEVALVIND dei motori
+TODO: tipo lunghezza asse uguale a lunghezza feedback
+TODO: supporti laterali GRAD
+
 """

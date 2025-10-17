edc21bb9b1307f6019f7e6137b0e339157be1abb
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2025-10-17 16:42:58 +0200

    Update data_config.py

--- utils/db/data_config.py
+++ utils/db/data_config.py
@@ -929,14 +929,52 @@ def run_io_expr_scan(iotype: int, ind_target: int = None):
                     if not_val in [IO_EXPR_NONE, IO_EXPR_VAL, IO_EXPR_NOTVAL]:
                         group_num = ((i - 1) // 3) + 1
                         if opnd_val == ind_target:
-                            print(f"IO\t→\tDI\t→\t[{Ind}]{get_io_name(iotype=IO_DI, Ind=Ind)}\t→\tExpr N{group_num}")
+                            print(f"IO\t→\tDI\t→\t[{Ind}] {get_io_name(iotype=IO_DI, Ind=Ind)}\t→\tExpr N{group_num}")
+        for Ind in range(0, len(data_config.IO_RI_List)):
+            if data_config.IO_DI_List[Ind].intval[IO_INT_ADDRTYPE] in [IO_TYPE_FUNC_TOT, IO_TYPE_FUNC_TOTAUTO,
+                                                                       IO_TYPE_FUNC_TOTMAN, IO_TYPE_FUNC_DTOT,
+                                                                       IO_TYPE_FUNC_DTOTAUTO, IO_TYPE_FUNC_DTOTMAN,
+                                                                       IO_TYPE_FUNC_TIME, IO_TYPE_FUNC_TIMEAUTO,
+                                                                       IO_TYPE_FUNC_TIMEMAN, IO_TYPE_FUNC_DTIME,
+                                                                       IO_TYPE_FUNC_DTIMEAUTO, IO_TYPE_FUNC_DTIMEMAN]:
+                -
+                if len(data_config.IO_DI_List[Ind].exprintval) <= 1:
+                    continue
+
+                expr_type = data_config.IO_DI_List[Ind].exprintval[0]
+
+                # 🔁 Ciclo sui gruppi (partendo da index 1, passo di 3)
+                for i in range(1, len(data_config.IO_DI_List[Ind].exprintval), 3):
+                    if i + 2 >= len(data_config.IO_DI_List[Ind].exprintval):
+                        if DEBUG_DEBUG_DEBUG:
+                            print('xERR_001')
+                        continue
+                    not_val, opnd_val, oper_val = data_config.IO_DI_List[Ind].exprintval[i:i + 3]
+                    if not_val in [IO_EXPR_NONE, IO_EXPR_VAL, IO_EXPR_NOTVAL]:
+                        group_num = ((i - 1) // 3) + 1
+                        if opnd_val == ind_target:
+                            print(f"IO\t→\tRI\t→\t[{Ind}] {get_io_name(iotype=IO_DI, Ind=Ind)}\t→\tExpr N{group_num}")
+
+
+def run_io_scan(iotype: int, ind_target: int = None):
+    if iotype == IO_DI:
+        for Ind in range(0, len(data_config.IO_DI_List)):
+            if data_config.IO_DI_List[Ind].intval[IO_INT_ADDRTYPE] == IO_TYPE_CALC:
+                # CAMPO TIMEOUT DEI DI
+                if data_config.IO_DI_List[Ind].intval[IO_INT_TIMEOUT] == -1:
+                    continue
+                delay_di = data_config.IO_DI_List[Ind].intval[IO_INT_ININD]
+                if delay_di and delay_di == ind_target:
+                    print(f"IO\t→\tDI\t→\t[{Ind}] {get_io_name(iotype=IO_DI, Ind=Ind)}\t→\tIn")
+            run_io_expr_scan(iotype=IO_DI, ind_target=Ind)
 
 
 def run_io_search(iotype: int, Ind: Optional[int] = None):
     if iotype == IO_DI:
-        run_params_scan(iotype=IO_DI, Ind=Ind)
-        run_axis_scan(iotype=IO_DI, axisInd=None, Ind=Ind)
-        run_io_expr_scan(iotype=IO_DI, ind_target=Ind)
+        run_params_scan(iotype=IO_DI, Ind=Ind)  # PARAMETRI MACCHINA
+        # run_motor_scan(iotype=IO_DI, Ind=Ind)
+        run_axis_scan(iotype=IO_DI, axisInd=None, Ind=Ind)  # ASSE
+        run_io_scan(iotype=IO_DI, ind_target=Ind)  # IO DI
     elif iotype == IO_DO:
         # ============================================
         # 🧠 Campi generici configurazione nei -out

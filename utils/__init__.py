cbaff47fa99a1190528b87edfcfeac5084c0c5d6
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2025-11-01 14:44:11 +0100

    bump

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,4 @@
 """
-# TODO: comando refresh (a mano se no una volta lanciato sovrascrive config_old)
-# TODO: rinominare config > config_old
 # TODO: controllo differenze config > config_old
 # TODO: verifiche (verificare che per i reset pressostati si scende solo con il joystick, se ci sono digitali nel fb_err)
 # TODO: veridicare che la variabile di sistema sia su "SI" il booleano in caso di assi se no non VA (L LL L0 EEC)

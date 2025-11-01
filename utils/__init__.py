530c503fa0e027bff9baac8c2d17b6223a012c95
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2025-11-01 23:07:35 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,10 @@
 """
-# TODO: controllo differenze config > config_old
+NON POSSIBILI
 # TODO: verifiche (verificare che per i reset pressostati si scende solo con il joystick, se ci sono digitali nel fb_err)
+
 # TODO: veridicare che la variabile di sistema sia su "SI" il booleano in caso di assi se no non VA (L LL L0 EEC)
-# TODO: caricare tutte le informazioni asse per asse
+
+POSSIBILI
 # TODO: controllo per vedere se un uscita DO o AO è usata su piu assi
 # TODO: controllo se c è un SYSTEM di usasse usato ma c'è il FLAG su NO
 # TODO: controllo safety interlock sgancio chiusura, non deve esserci Droll L (o la system)

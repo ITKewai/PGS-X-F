0d4bab02deb9bcc390a19b91a15cb85eeb002fdb
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2025-11-05 13:50:39 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -9,7 +9,8 @@ POSSIBILI
 # TODO: notificare se le termiche sono state inserite nei motori (no resistanza)
 
 # TODO: controllo geomatria, width deve essere coerente con modello macchina, anche diametro
-
+# TODO: in base a indirizzo ip scheda rete imposta ip plc e scarica config
+# TODO: controllare che allarmi siano safetystop
 # TODO: verificare che la velocità dei rulli sia ugale in control per master e slave
 # TODO: verificare che master mult sia 5 e vmin infeiriore  a 3
 # TODO: quota di reset maggiore a quota apertura sgancio
@@ -21,7 +22,7 @@ POSSIBILI
 # TODO: verificare che input non ha seq imposto
 
 # TODO: rendere questo programma un server web anche cosi si integra nell'hmi ,fare vedere allarmi a lato cliccabili per errori console
-
+#TODO: controllo che rotazione H SIA 500.000 E L -500
 INCERTI:
 # TODO: va in errore se cerco system si patcha separando la ricerca
 #TODO: TEST FINALE RICERCA IN OGNI CAMPO VISIBILE

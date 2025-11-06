36b3fb7f19f81d4b741437f008efd1465efb8dd5
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2025-11-06 10:14:08 +0100

    Update version.py

--- utils/version.py
+++ utils/version.py
@@ -4,7 +4,7 @@
 Modulo che contiene le informazioni di versione e metadati del progetto PSG-X-FindIndex.
 """
 
-__version__ = "1.0.3.2"
+__version__ = "1.0.3.3"
 __pgs_version__ = "0.25.42.0"
 __author__ = "ITKewai"
 __company__ = ""

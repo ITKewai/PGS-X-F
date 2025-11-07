f202e321d9ea3db71d91a858943ee1276030cea9
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2025-11-07 15:19:21 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -22,5 +22,15 @@ A TEMPO PERSO:
 # TODO: in base a indirizzo ip scheda rete imposta ip plc e scarica config
 # TODO: verifiche (verificare che per i reset pressostati si scende solo con il joystick, se ci sono digitali nel fb_err)
 
+# TODO: [ASSE 10] TILT
+#   ⚠️  Flag HH disattivo ma HH=SYS è impostato/usato
+#     ↳ Axes	→	[10]TILT	→	HH
+#         ↳ IO	→	DI	→	[99] TopRollRotation RI Enable Count	→	Expr	→	N0
+#         ↳ IO	→	DI	→	[100] TopRollRotation RI Reset	→	Expr	→	N0
+#         ↳ IO	→	DI	→	[307] None	→	Expr	→	N0
+#         ↳ IO	→	DI	→	[308] None	→	Expr	→	N0 fuori indice???
 
+⚠️  Duplicati trovati in DI:
+   → PNET 0.3   → [64] EMERGENCY RESET PB, [272] EMERGENCY RESET PB
+   272 NON ESISTE
 """

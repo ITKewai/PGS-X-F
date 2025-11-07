1cecc600a88dcbf104545094fa0c4da2db489d9f
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2025-11-07 08:58:03 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,14 +1,11 @@
 """
-NON POSSIBILI
-# TODO: verifiche (verificare che per i reset pressostati si scende solo con il joystick, se ci sono digitali nel fb_err)
+
 
 
 POSSIBILI
 # TODO: notificare se le termiche sono state inserite nei motori (no resistanza)
 
-# TODO: controllo geomatria, width deve essere coerente con modello macchina, anche diametro
 # TODO: controllare che allarmi siano safetystop
-# TODO: verificare che la velocità dei rulli sia ugale in control per master e slave
 # TODO: verificare che master mult sia 5 e vmin infeiriore  a 3
 
 # TODO: verifica che indirizzi KE1/KE4 non sono scritti
@@ -23,5 +20,7 @@ INCERTI:
 
 A TEMPO PERSO:
 # TODO: in base a indirizzo ip scheda rete imposta ip plc e scarica config
+# TODO: verifiche (verificare che per i reset pressostati si scende solo con il joystick, se ci sono digitali nel fb_err)
+
 
 """
